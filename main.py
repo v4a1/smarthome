158b7113ece90b544d3ccc4fea00a6fcd66c2ebd
Некрасов Валентин <valentin.fergana@gmail.com>
2019-02-08 19:48:17 +0700

    work

--- main.py
+++ main.py
@@ -20,12 +20,57 @@ class MainWindow:
         self.date.pack()
         self.date.after_idle(self.tick)
 
+        self.addmeter = Button(master, text='Добавить счетчик', command= lambda: AddMeter(self))
+        self.addmeter.pack()
+
+        self.meter_frame = Frame()
+        self.meter_frame.pack()
+
+        meters = [{'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}, {'title': 'Горячая вода', 'indication':'000135481', 'last_indication':'000134856', 'last_pay_date':'1.02.2019', 'cost':'15'}]
+
+        for i in meters:
+            self.i = Meter(self.meter_frame, i)
+
+        #cold_water_meter = {'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}
+        #self.cold_water_meter = Meter(self.meter_frame, cold_water_meter)
+
+        #hot_water_meter = {'title': 'Горячая вода', 'indication':'000135481', 'last_indication':'000134856', 'last_pay_date':'1.02.2019', 'cost':'15'}
+        #self.hot_water_meter = Meter(self.meter_frame, hot_water_meter)
+
     def tick(self):
-        self.date.after(500, self.tick)
+        self.date.after(1000, self.tick)
         dt = datetime.now()
         self.date['text']=dt.strftime("%H:%M:%S %d %B %Y")
+    
+    def add_meter(self):
+        pass
 
+class Meter(Frame):
+    def __init__(self, master, date):
+        Frame.__init__(self, master)
+        self.pack(side=LEFT)
+        self['width'] = 200
+        self['height'] = 300
+        self['bg'] = 'black'
+        self['bd'] = 5
+        self.label = Label(self, text=date.get('title'))
+        self.label.pack()
+        self.lebel_indication = Label(self, text='Текущие')
+        self.lebel_indication.pack()
+        self.indication = Label(self, text=date.get('indication'), font='arial 24')
+        self.indication.pack()
+        self.last_pay_date = Label(self, text='Оплачены '+date.get('last_pay_date'))
+        self.last_pay_date.pack()
+        self.last_indication = Label(self, text=date.get('indication'))
+        self.last_indication.pack()
+        self.pay = Label(self, text='Нужно заплатить ' + str((int(date.get('indication')) - int(date.get('last_indication'))) * int(date.get('cost'))) + ' Рублей')
+        self.pay.pack()
 
+class AddMeter():
+    def __init__(self, main_window):
+        self.main = main_window
+        self.windows = Toplevel(main_window.master)
+        self.windows.geometry("400x300+{}+{}".format(self.main.screenwidth//2-200, self.main.screenheight//2-150))
 
 
 
