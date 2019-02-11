3c102b84d99f892107782f244a39fa28189f023c
Valentin Nekrasov <valentin.fergana@gmail.com>
2019-02-12 00:14:28 +0700

    work

--- main.py
+++ main.py
@@ -38,12 +38,11 @@ class MainWindow:
             x = Frame()
             self.meters_frame.append(x)
             self.meters_frame[i].pack()
-        self.addmeter = Button(text='Настройка счетчиков', command=self.meterconfig)
+        self.addmeter = Button(text='Настройка счетчиков', command=self.metersconfig)
         self.addmeter.pack()
-        self.meters = [{'activ':1,'activ':1,'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}, {'activ':1,'activ':1,'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}, {'activ':1,'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}, {'activ':1,'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}, {'activ':1,'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}, {'activ':1,'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}, {'activ':1,'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}, {'activ':1,'title': 'Горячая вода', 'indication':'000135481', 'last_indication':'000134856', 'last_pay_date':'1.02.2019', 'cost':'15'}]
+        self.meters = [{'title': 'Холодная вода', 'indication':'00000001', 'last_indication':'00000001', 'last_pay_date':'1.02.2019', 'cost':'15'}, {'title': 'Горячая вода', 'indication':'000135481', 'last_indication':'000134856', 'last_pay_date':'1.02.2019', 'cost':'15'}]
         x = 0
         for i in self.meters:
-            print(x)
             if x < 4:
                 self.i = Meter(self.meters_frame[0], i)
             else:
@@ -51,8 +50,8 @@ class MainWindow:
             x += 1
 
 
-    def meterconfig(self):
-        MeterConfig(self)
+    def metersconfig(self):
+        MetersConfig(self)
 
 class Meter(Frame):
     def __init__(self, master, date):
@@ -75,7 +74,7 @@ class Meter(Frame):
         self.pay = Label(self, text='Нужно заплатить ' + str((int(date.get('indication')) - int(date.get('last_indication'))) * int(date.get('cost'))) + ' Рублей')
         self.pay.pack()
 
-class MeterConfig():
+class MetersConfig():
     def __init__(self, main_window):
         self.main = main_window
         self.window = Toplevel(self.main.master)
@@ -116,49 +115,39 @@ class MeterConfig():
             meter[index]['cost'].grid(row=x, column=6)           
             index += 1
             x += 1
-        add = Button(self.window, text='Добавить')
-        add.grid(row=x, column=4) 
+        add = Button(self.window, text='Добавить', command=lambda index=[9]: self.meter_edit(index))
+        add.grid(row=x, column=1, columnspan=2) 
         edit = Button(self.window, text='Редактировать')
-        edit.grid(row=x, column=5) 
+        edit.grid(row=x, column=3, columnspan=2) 
         delete = Button(self.window, text='Удалить') 
-        delete.grid(row=x, column=6) 
+        delete.grid(row=x, column=5, columnspan=2) 
 
-    def test(self):   
-        self.title = Frame(self.window)
-        self.title.pack()
-        tlabel = Label(self.title, text="Заголовок", width=20)
-        tindication = Label(self.title, text="Показания", width=20)
-        tcost = Label(self.title, text='Цена', width=20)
-        tadd = Button(self.title, text="Добавить", width=20)
-        tlabel.pack(side=LEFT)
-        tindication.pack(side=LEFT)
-        tcost.pack(side=LEFT)
-        tadd.pack(side=LEFT)
+    def meter_edit(self, index):
+        window = Toplevel(self.window)
+        window.geometry("400x300+{}+{}".format(self.main.screenwidth//2-200, self.main.screenheight//2-150))
+        window.columnconfigure(1, weight=1)
+        window.columnconfigure(2, weight=1)
+        Label(window, text='Новый счетчик').grid(row=0, column=1, columnspan=2)
+        x = []
+        meters_titles = ['', 'Заголовок', 'Оплата', 'Показания', 'Источник', 'Цена']
+        for i in range(1,6):
+            l = Label(window, text=meters_titles[i]).grid(row=i, column=1)
+            x.append(Entry(window).grid(row=i, column=2)) 
         
-        x = 0
-        for i in self.main.meters:
-            x += 1
-            self.meter = Frame(self.window)
-            self.meter.pack()
-            title = Entry(self.meter, width=20)
-            indication = Entry(self.meter, width=20)
-            cost = Entry(self.meter, width=20)
-            delmeter = Button(self.meter, width=20, text='Удалить', command= lambda: self.delmeter(x))
-            title.pack(side=LEFT)
-            indication.pack(side=LEFT)
-            cost.pack(side=LEFT)
-            delmeter.pack(side=LEFT)
-            title.insert(0, i.get('title')) 
-            indication.insert(0, i.get('last_indication'))
-            cost.insert(0, i.get('cost'))
+            
+
+ 
+        
+
+
+
+
     def delmeter(self, index):
         self.main.meters.remove(index)
         print(self.main.meters)
 
 
-            
 
-            
 
 def main():
     root = Tk()
